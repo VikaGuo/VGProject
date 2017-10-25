e7e7d219d1ef1087a07406d0d7614d0a1f542394
Vika Guo <flex.guo@flex.com>
2017-10-25 01:57:37 -0700

    [Studio] Modified screen "reportissue"

--- client/controllers/app/reportissue.js
+++ client/controllers/app/reportissue.js
@@ -2,4 +2,8 @@ angular.module('app').controller('app_reportissue', app_reportissue);
 function app_reportissue($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.save = function() {
+        app.action('reportissue', 'save', this)
+    }
 }
